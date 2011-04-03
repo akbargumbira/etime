c94199e4bcc430adfe5489ec598c03a158dc4913
imajimatika@gmail.com <imajimatika@gmail.com@bf5e7cf4-e708-25e7-cb3c-aeecc9589e34>
2011-04-03 06:04:49 +0000

    bsa log in, gak bisa log out... <|==|>

--- eTime/eTime/Login.xaml.cs
+++ eTime/eTime/Login.xaml.cs
@@ -24,5 +24,42 @@ namespace eTime
         {
             NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
         }
+
+        private void wbLogin_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            string strLoweredAddress = e.Uri.OriginalString.ToLower();
+            if (strLoweredAddress.StartsWith("http://www.facebook.com/connect/login_success.html?code="))
+            {
+                txtStatus.Text = "trying to retrieve access token";
+                wbLogin.Navigate(HelperClasses.FBUri.GetTokenLoadUri(e.Uri.OriginalString.Substring(56)));
+                return;
+            }
+            string strTest = wbLogin.SaveToString();
+            if (strTest.Contains("access_token"))
+            {
+                int nPos = strTest.IndexOf("access_token");
+                string strPart = strTest.Substring(nPos + 13);
+                nPos = strPart.IndexOf("</PRE>");
+                // removes expires
+                nPos = strPart.IndexOf("&expires=");
+                if (nPos != -1)
+                {
+                    strPart = strPart.Substring(0, nPos);
+                }
+                //strPart = strPart.Substring(0,nPos);
+                App.AccessToken = strPart;
+                txtStatus.Text = "Authenticated - you will redirect to eTime";
+                txtError.Text = "OK";
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                return;
+            }
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            wbLogin.Navigate(HelperClasses.FBUri.GetLoginUri());
+        }
+
+
     }
 }
